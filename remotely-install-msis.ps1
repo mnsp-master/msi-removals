cc6c7c090986db95706787953855d72cc66478aa
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-02-17 15:14:51 +0000

    20

--- remotely-install-msis.ps1
+++ remotely-install-msis.ps1
@@ -1,9 +1,11 @@
-# mnsp-ver 0.0.0.0.19
+# mnsp-ver 0.0.0.0.20
 Clear-Host
 
 $sourcefile = Read-Host "UNC Path to source msi... e.g \\server1\share\install.msi"
 $SourceMSI = $sourcefile.split("\")[-1]
 $destinationFolder = "C:\Temp"
+$installer = "$destinationFolder\$SourceMSI"
+$log = "$destinationFolder\$SourceMSI.log"
 
 do { # loop until user selects 2 to quit - begin
 
@@ -26,24 +28,14 @@ do { # loop until user selects 2 to quit - begin
 				Copy-Item -Path $sourcefile -Destination $destinationFolder -Verbose
 
 				$session = New-PSSession -computername $computername
-				$session
+				$session				
 
-				
-				#Invoke-Command -Session $session -ScriptBlock { Copy-Item -Path $using:sourcefile -Destination $using:destinationFolder -Verbose}
-				#$installer = "$using:destinationFolder\$using:SourceMSI"
-				#$log = "/l* $using:destinationFolder\$using:sourceMSI.log"
-
-				$installer
-				$log
-				
-				Invoke-Command -Session $session -ScriptBlock { $SN02=(Start-Process msiexec -ArgumentList '/x', $using:installer, '/q', $using:log -wait -PassThru)
-				Write-Host "Exitcode:" $SN02.ExitCode
+				Invoke-Command -Session $session -ScriptBlock { $SN02=(Start-Process msiexec -ArgumentList '/i', $using:installer, '/q', $using:log -wait -PassThru)
+				Write-Host "MSI Exitcode:" $SN02.ExitCode
 				}
 
 				Remove-PSSession $session
 
-				#Write-Host "invoking msiexec on remote computer $computername ..."
-				#Invoke-Command -ComputerName $computername -ScriptBlock { Msiexec /i C:\Temp\$sourceMSI /qb }
 				Write-Host "--------------------------------------------------------------------- `n"
 				#>
 			}		
@@ -52,3 +44,13 @@ do { # loop until user selects 2 to quit - begin
 
 }until ($choice -eq 2) # loop until user selects 2 to quit - end
 Write-Host "Bye...."
+
+#Invoke-Command -Session $session -ScriptBlock { Copy-Item -Path $using:sourcefile -Destination $using:destinationFolder -Verbose}
+#$installer = "$using:destinationFolder\$using:SourceMSI"
+#$log = "/l* $using:destinationFolder\$using:sourceMSI.log"
+
+#$installer
+#$log
+
+#Write-Host "invoking msiexec on remote computer $computername ..."
+#Invoke-Command -ComputerName $computername -ScriptBlock { Msiexec /i C:\Temp\$sourceMSI /qb }

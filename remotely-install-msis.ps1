6c2051203f4e2ef63dd01ac566582446c613d6c8
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-02-17 11:19:51 +0000

    test2

--- remotely-install-msis.ps1
+++ remotely-install-msis.ps1
@@ -1,7 +1,9 @@
 #
 Clear-Host
-$sourcefile = "\\server\script\CrystalDiskInfo7.0.4.msi" #update UNC path as necessary
+#$sourcefile = "\\server\script\CrystalDiskInfo7.0.4.msi" #update UNC path as necessary
+$sourcefile = Read-Host "Path to source msi..."
 
+Read-Host "Please supply credentials when prompted, press any key to continue..."
 $domaincredentials = Get-Credential
 
 
@@ -9,7 +11,7 @@ do {
 
 Write-Host "1. Remotely install msi"
 Write-host "2. Quit"
-$choice=Read-Host "Chose a number to continue"
+$choice = Read-Host "Chose a number to continue"
 
 switch ($choice)
 {
@@ -18,13 +20,16 @@ switch ($choice)
 	#$computername = 'pc-1', 'pc-2'
 
 	$destinationFolder = "\\$computer\C$\Temp"
-	
+	<#
 	if (!(Test-Path -path $destinationFolder -Credential $domaincredentials)) {
 		New-Item $destinationFolder -Type Directory -Credential $domaincredentials
 	}
 	Copy-Item -Path $sourcefile -Destination $destinationFolder -Credential $domaincredentials
 	Invoke-Command -ComputerName $computer -Credential $domaincredentials -ScriptBlock { Msiexec /i C:\Temp\$sourcefile /qb }
+	#>
  }
 
-} until ($choice -eq 2)
- Write-Host "Bye...."
+} 
+
+}until ($choice -eq 2)
+Write-Host "Bye...."

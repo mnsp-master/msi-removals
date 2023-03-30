9fb9857428c63871a5dd9663f98bfd4e839ce245
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2023-03-30 11:53:24 +0100

    additional app path detection

--- uninstall-sims.net-client-if-exists.ps1
+++ uninstall-sims.net-client-if-exists.ps1
@@ -5,15 +5,21 @@ $mnspver = "1.0.0.0.5"
 $LogDir = "C:\Temp\MNSP"
 $transcriptlog = "$LogDir\$(Get-date -Format yyyyMMdd-HHmmss)_Sims.netClient_Remove_transcript.log"
 $sleep = "20"
-$AppPath = "C:\Program Files (x86)\SIMS\Sims .net"
+$AppPathSIMS = "C:\Program Files (x86)\SIMS\Sims .net"
+$AppPathOpenVPN = "C:\Program Files\OpenVPN"
+$AppPathScomisHosted = "C:\Program Files (x86)\Scomis\Hosted Applications"
 $StartMenuContent = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\SIMS Applications"
+
 $software = "Sims .Net Client"
 Start-Transcript -Path $transcriptlog
 function DottedLine {
 Write-Host "-----------------------------------------------------------------------------------------------`n"
 } 
 DottedLine
-    If(Test-Path $AppPath ) {
+
+if(!((Test-Path $AppPathScomisHosted ) -and (Test-Path $AppPathOpenVPN))) { #if neither of the other app paths exist proceed with removal...
+
+    If(Test-Path $AppPathSIMS ) {
         Write-Host "'$software' is installed, procedding with removal..."
 
             # get pulsar process - force kill off if currently running...
@@ -28,7 +34,7 @@ DottedLine
             Remove-Variable pulsar
             start-sleep $sleep
 
-        Remove-Item $AppPath -Recurse -Force -Verbose #force delete orphaned FS content
+        Remove-Item $AppPathSIMS -Recurse -Force -Verbose #force delete orphaned FS content
         Write-Host "Sleeping for $sleep seconds..."
         start-Sleep $sleep
         Remove-Item $StartMenuContent -Recurse -Force -Verbose #force delete orphaned FS content
@@ -37,9 +43,17 @@ DottedLine
         Write-Host "$software' is not installed, exiting with no further actions..."
     }
 DottedLine
+}
 Stop-Transcript
 
 <#
+Question
+Sign in to vote
+4
+Sign in to vote
+if( !( ( test-path c:\install\foo.bar ) -and ( test-path c:\install\test.txt ) ) ){
+"one of the files not found!"; break
+
 start-Sleep $sleep
 & $command $arguments # execute uninstaller
 $installed = (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where { $_.DisplayName -eq $software }) -ne $null #checks for uninstall string from registry

ac05c166b1d5e1593356683000043e8f51d32176
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-03-19 16:13:54 +0000

    16:13

--- uninstall-fusion-agent-rmmc-prep.ps1
+++ uninstall-fusion-agent-rmmc-prep.ps1
@@ -2,34 +2,31 @@
 #used in conjunction with emco package builder, to create necessary operating folder/path and distribute/execute this powershell script.
 #
 clear-host
-$MNSPver = "0.0.1"
+$MNSPver = "1.0.2"
 $LogDir = "C:\Temp\MNSP"
-$transcriptlog = "$LogDir\$(Get-date -Format yyyyMMdd-HHmmss)_FusionRemove_transcript.log"
+$transcriptlog = "$LogDir\$(Get-date -Format yyyyMMdd-HHmmss)_FusionRemove_RMMC_Prep_transcript.log"
 $sleep = "60"
-$ProcessToKIll = "fusioninventory-agent_windows-x64_2.6"
-$RetryCount = 3
+$ProcessToKIll = "notepad"
+$RetryCount = 2 #note attempt 1 will be counted as 0
 
 Start-Transcript -Path $transcriptlog
 function DottedLine {
 Write-Host "-----------------------------------------------------------------------------------------------`n"
 } 
-
 DottedLine
-
 do { #loop for n times
     $attempt = $RetryCount - ($RetryCount -$_)
-
-        $process = Get-Process "$ProcessToKill"
+    $process = Get-Process "$ProcessToKill" -ErrorAction Ignore
         if ( $process ) {
-
         Write-host "Process running force killing..."
         Write-host "Loop number:" $attempt
         Stop-Process $Process -Force -Verbose
-        sleep 60
-
+        Start-Sleep $sleep 
         }
     $_++ #Increment loop counter
+    Start-Sleep $sleep # additional sleep - just in case process has begun after script has started
 } until ($attempt -gt $RetryCount)
 
 DottedLine
 Stop-Transcript
+

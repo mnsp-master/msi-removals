e9340369b13df33721946e8f764951ede02f0aff
mnsp-snoble <116190113+mnsp-snoble@users.noreply.github.com>
2024-03-20 08:11:13 +0000

    08:11

--- uninstall-fusion-agent-rmmc-prep.ps1
+++ uninstall-fusion-agent-rmmc-prep.ps1
@@ -2,11 +2,11 @@
 #used in conjunction with emco package builder, to create necessary operating folder/path and distribute/execute this powershell script.
 #
 clear-host
-$MNSPver = "1.0.5"
+$MNSPver = "1.0.6"
 $LogDir = "C:\Temp\MNSP"
 $transcriptlog = "$LogDir\$(Get-date -Format yyyyMMdd-HHmmss)_FusionRemove_RMMC_Prep_transcript.log"
 $sleep = "60"
-$ProcessToKIll = "fusioninventory-agent_windows-x64_2.6.exe"
+$ProcessToKIll = "fusioninventory-agent_windows-x64_2.6"
 $RetryCount = 2 #note attempt 1 will be counted as 0
 
 Start-Transcript -Path $transcriptlog
